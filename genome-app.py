c04942ce7cd758fce1ff5c1b6b1e291d9d8dd27e
Zeeshan Javed <zeeshanjaved28@hotmail.com>
2025-05-23 19:48:35 +0500

    Updated File

--- genome-app.py
+++ genome-app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 # Load your trained models (replace with your actual paths)
 genetic_disorder_model = joblib.load('models\\Genetic_Disorder_Stacked_Model.pkl.gz')
-disorder_subclass_model = joblib.load('models\\Disorder_Subclass_Stacked_Model.pkl.gz')
-combined_disorder_model = joblib.load('models\\Combined_Genome_Disorder_Stacked_Model.pkl.gz')
+disorder_subclass_model = joblib.load('models\\Disorder_Subclass_Stacked_Model.pkl.xz')
+combined_disorder_model = joblib.load('models\\Combined_Genome_Disorder_Stacked_Model.pkl.xz')
 
 # Features lists for each model (in the order they were trained)
 features_genetic = [
